a41b487ed67e950aa232b3d1db63074b526c6e4f
kristen-sauby <48966037+kristen-sauby@users.noreply.github.com>
2019-04-01 15:17:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# R_SQL_Workflows
---
+++ code/fake_database.R
@@ -0,0 +1,76 @@
+# create fake data about the Eastern Jackalope
+
+# JackalopeInfo
+JackalopeInfo <- as.data.frame(cbind(
+	ID_number = 1:6,
+	sex = rep(c("M","F"),3),
+	Site = c(
+		rep("Paynes Prairie", 2),
+		rep("San Felasco", 2),
+		rep("Ocala National Forest", 2)
+	)
+))
+
+# JackalopeSurveys
+JackalopeSurveys <- expand.grid(
+	ID_number = 1:6,
+	Survey_Date = seq(as.Date("2000/1/1"), by = "month", length.out = 6)
+) %>%
+	arrange(ID_number)
+
+JackalopeSurveys$Weight <- c(
+	runif(6,min=8100,max=8200),
+	runif(6,min=8400,max=8500),
+	runif(6,min=8900,max=9000),
+	runif(6,min=9400,max=9500),
+	runif(6,min=5000,max=5100),
+	runif(6,min=6600,max=6700)
+)
+
+ggplot(
+	JackalopeSurveys, 
+	aes(
+		Survey_Date, Weight, shape = factor(ID_number))) + 
+	geom_point(aes(colour = factor(ID_number)), size = 4) +
+	geom_point(colour = "grey90", size = 1.5)
+
+
+
+ExporttoACCDB <- function (
+	database.name = "C:/Users/Kristen.Sauby/Documents/Projects/Misc/DatabaseTemplate.accdb", 
+	x, 
+	tablename = as.character(1:length(x)),
+	...
+){
+	mycon <- dbConnect(
+		drv = odbc::odbc(), 
+		.connection_string = paste(
+			"Driver={Microsoft Access Driver (*.mdb, *.accdb)}; Dbq=",
+			database.name,
+			sep=""
+		)
+	)
+	if (class(x) == "data.frame") 
+		dbWriteTable(mycon, x, tablename = tablename[1])
+	else for (i in 1:length(x)){
+		dbWriteTable(mycon, x, tablename = tablename[1])
+	} 
+	close(mycon)
+}
+
+
+
+#install.packages("ImportExport")
+
+library(ImportExport)
+ExporttoACCDB(
+	database.name = "C:/Users/Kristen.Sauby/Documents/Projects/Misc/DatabaseTemplate.accdb", 
+	x=list(JackalopeInfo)
+)
+
+
+
+
+
+ImportExport::access_export("C:/Users/Kristen.Sauby/Documents/Projects/Misc/DatabaseTemplate.accdb",as.data.frame(JackalopeInfo),
+					   tablename="JackalopeInfo")
